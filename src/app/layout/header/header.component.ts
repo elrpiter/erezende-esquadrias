116c0e004d469b17de89a540ac51397eb4c43cd5
Piter Lourencini <piter_elr@hotmail.com>
2026-01-21 20:28:06 -0300

    Ajustes no header e remoção de imagem não utilizada

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core'; // Importe o HostListener
 import { RouterModule } from '@angular/router';
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  isScrolled = false; // Propriedade para controlar o estado
+
+  // O HostListener "escuta" eventos do navegador, neste caso, o 'scroll' da janela
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Se o scroll vertical for maior que 10 pixels, a propriedade vira true
+    this.isScrolled = window.scrollY > 10;
+  }
 
 }
